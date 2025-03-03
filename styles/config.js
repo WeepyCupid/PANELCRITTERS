4ffa0872ea8faa057b3ef4fe6d2c06e4405d1a42
WeepyCupid <114519932+WeepyCupid@users.noreply.github.com>
2025-03-03 15:29:07 -0600

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1F3icMYYb9w3gZBp7z10dcQsbJ14_tMf4moTk94swnmw";
 
 
 /* ------------------------------------------------------------------- */
